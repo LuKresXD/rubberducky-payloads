89f4e2b07d79e57b02149dd268ea8469f0b39062
LuKres <lukres006@gmail.com>
2024-09-08 17:00:44 +0300

    Footer.tsx changed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 export default function Footer() {
     return (
         <>
-            <footer className="max-w-4xl w-full flex flex-col mx-auto pt-4 pb-2">
+            <footer>
                 <div className="h-0.5 w-full rounded-lg bg-gradient-to-r from-secondary via-accent to-secondary"/>
                 <h2 className="font-leaguespartan text-center font-semibold text-base text-text pt-2">
-                    payloads.lukres.dev - Made with NextJS, TailwindCSS, and ❤ by me
+                    phishing.lukres.dev - Made with NextJS, TailwindCSS, and ❤ by Luka
                 </h2>
             </footer>
         </>
